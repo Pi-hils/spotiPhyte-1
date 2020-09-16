15b1c87fe44b9b54397a3583d5bde00802c94607
Alexis Simonian <alexis792.as@gmail.com>
2020-09-16 15:43:34 +0200

    login w/ spotify

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require( 'express' );
 const app = express();
+const my_client_id = '5da38576975e4705976cf5174775d9a5'
+const redirect_uri = 'http://localhost:3000/callback'
 
 app.use( express.static( 'public' ));
 
@@ -9,4 +11,17 @@ app.listen( 3000, function() {
 
 app.get( '/', ( req, res ) => {
   res.sendFile( 'index.html' );
+});
+
+app.get( '/login', (req, res) => {
+  let scopes = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize' +
+  '?response_type=code' +
+  '&client_id=' + my_client_id +
+  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+  '&redirect_uri=' + encodeURIComponent(redirect_uri));
+});
+
+app.get( '/callback', (req, res) => {
+  res.redirect( '/' );
 });
