318cd40275c778dfc2cae4cb7e88db3be1ca5329
Hilda Amponsah <hilda.amp@hotmail.com>
2020-09-22 22:44:04 +0100

    all goods and prices added, undefined after input shop in cmdline

--- public/shop.js
+++ public/shop.js
@@ -16,13 +16,18 @@ function displayGoods() {
 
 function buy(selection) {
   for (var i = 0; i < goods.length; i++) {
-    // if(Object.values(goods).includes(selection)){
+    // throw "insufficient gems" if(gems < good[i].price){
     if((goods[i].item.includes(selection))){ 
-   gems -= goods[i].price;
+      gems -= goods[i].price;
       return "item purchased"
-
     } else if((goods[1].item.includes(selection))){ 
       gems -= goods[1].price;
+        return "item purchased"
+    } else if((goods[2].item.includes(selection))){ 
+      gems -= goods[2].price;
+       return "item purchased"
+    } else if((goods[3].item.includes(selection))){ 
+      gems -= goods[3].price;
         return "item purchased"
       } else {
       return `${selection} is not valid`;
