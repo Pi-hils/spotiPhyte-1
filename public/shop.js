cbaba52b777bcf47e361dbba5efae1bbc3d69c60
Hilda Amponsah <hilda.amp@hotmail.com>
2020-09-22 15:29:37 +0100

    add more items

--- public/shop.js
+++ public/shop.js
@@ -2,12 +2,18 @@ let gems = 10 //amount of gems user has
 let goods = [{
   item: "fertilizer",
   price: 1
+},{
+  item: "plant pot",
+  price: 2
+},{
+  item: "petels",
+  price: 4
 }];
 
 function displayGoods() {
   var i;
   for (i = 0; i < goods.length; i++) {
-    return `${goods[i].item}, price: ${goods[i].price}`
+    return `${goods.item}, price: ${goods.price}`
   }
   console.log(goods)
 }
